cf61ab0fcd36427e94c690e0158f41cfe6272c6e
Matthew Courtney <matthewdc182@gmail.com>
2020-11-21 18:55:35 -0500

    added in check to make sure that the user selects atleast one type of character

--- script.js
+++ script.js
@@ -17,11 +17,16 @@ function generatePassword() {
   var specialChar = confirm("Would you like to use special characters?");
   var length = prompt("Please enter a password length between 8-120 characters.")
 
-  
+// checks to make sure the length is infact a number and then checks to make sure if fits within the parameters
+
   if (isNaN(length) === true) {
     alert("Please enter a number.")
-} else if (length < 8 || lenghth > 120) {
+} else if (length < 8 || length > 120) {
     alert("Please choose a number between 8 and 120")
+}
+
+if (lowercase === false && uppercase === false && numbers === false && specialChar === false) {
+    alert("you must select atleast one type of character for your password")
 }
 }
 
